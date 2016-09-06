04498a83eee1a8391f953ec0725ddc7cb1180c93
Christian Butzke <chris@fruwe.com>
2016-09-06 11:04:36 +0900

    Resolve to_prepare deprecation warning in Rails 5.0

--- lib/haml_user_tags/rails/reloader.rb
+++ lib/haml_user_tags/rails/reloader.rb
@@ -3,8 +3,21 @@ module HamlUserTags
     initializer "haml_user_tags.enable_reloading" do
       watcher = ActiveSupport::FileUpdateChecker.new([], "#{Rails.root}/app/views/helpers" => %w{haml}) {}
       Rails.application.reloaders << watcher
-      ActionDispatch::Reloader.to_prepare do
+      reloader_class.to_prepare do
         watcher.execute_if_updated
+      end
+    end
+
+    private
+
+    def reloader_class
+      # Rails 5.0 says:
+      # DEPRECATION WARNING: to_prepare is deprecated and will be removed from Rails 5.1
+      # (use ActiveSupport::Reloader.to_prepare instead)
+      if ActiveSupport.version.release >= Gem::Version.new('5')
+        ActiveSupport::Reloader
+      else
+        ActionDispatch::Reloader
       end
     end
   end
