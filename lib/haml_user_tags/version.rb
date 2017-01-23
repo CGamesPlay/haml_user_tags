3bf02bac0f6ec88d27a3eae8b9707a9b2162dcf9
Ryan Patterson <cgamesplay@cgamesplay.com>
2017-01-23 14:47:05 -0800

    Increment version numbers

--- lib/haml_user_tags/version.rb
+++ lib/haml_user_tags/version.rb
@@ -1,4 +1,7 @@
 module HamlUserTags
-  VERSION = "0.10.2"
-  LATEST_STABLE_VERSION = "0.10.0"
+  # Corresponds to the newest unpublished version, aka the version the master
+  # branch will eventually become.
+  VERSION = "0.10.3"
+  # The most recent already-published version.
+  LATEST_STABLE_VERSION = "0.10.2"
 end
